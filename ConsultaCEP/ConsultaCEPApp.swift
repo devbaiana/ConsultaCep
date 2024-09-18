1d4648a2a2aa5cb5881064313cfb4d0820f87159
devbaiana <nath_karine@hotmail.com>
2024-09-18 19:04:22 +0100

    Initial Commit

---
+++ ConsultaCEP/ConsultaCEPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsultaCEPApp.swift
+//  ConsultaCEP
+//
+//  Created by nathalia karine on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConsultaCEPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
